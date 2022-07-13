8519a607d4d3bcb555dce77681f82e581d9da014
chenpei <1035788780@qq.com>
2022-07-13 21:44:39 +0800

    添加mapper注解

--- db/src/main/java/com/jenschen/dao/TaskMapper.java
+++ db/src/main/java/com/jenschen/dao/TaskMapper.java
@@ -2,7 +2,9 @@ package com.jenschen.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jenschen.entity.TaskEntity;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface TaskMapper extends BaseMapper<TaskEntity> {
 
 }
