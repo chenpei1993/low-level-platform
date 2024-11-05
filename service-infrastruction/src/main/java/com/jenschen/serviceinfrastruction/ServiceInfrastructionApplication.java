9aa322d4fbb8966225cba26bb659557912a00a41
jens <1035788780@qq.com>
2024-11-05 23:32:16 +0800

    添加spring cloud的基础模块

---
+++ service-infrastruction/src/main/java/com/jenschen/serviceinfrastruction/ServiceInfrastructionApplication.java
@@ -0,0 +1,13 @@
+package com.jenschen.serviceinfrastruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceInfrastructionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceInfrastructionApplication.class, args);
+    }
+
+}
---
+++ service-infrastruction/src/test/java/com/jenschen/serviceinfrastruction/ServiceInfrastructionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenschen.serviceinfrastruction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceInfrastructionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
