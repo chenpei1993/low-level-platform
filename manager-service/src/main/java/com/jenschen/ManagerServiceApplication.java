3e12fad8f3be985105df1d123af9e96f44beddc4
jens <1035788780@qq.com>
2024-11-08 14:25:09 +0800

    整合loadbalance功能

--- manager-service/src/main/java/com/jenschen/ManagerServiceApplication.java
+++ manager-service/src/main/java/com/jenschen/ManagerServiceApplication.java
@@ -3,7 +3,9 @@ package com.jenschen;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan(basePackages = "com.jenschen.mapper")
 public class ManagerServiceApplication {
--- service-gateway/src/main/java/com/jenschen/servicegateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/com/jenschen/servicegateway/ServiceGatewayApplication.java
@@ -2,7 +2,8 @@ package com.jenschen.servicegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 
 @SpringBootApplication
 public class ServiceGatewayApplication {
